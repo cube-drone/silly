be07121d6c8abdb3d032ce2a0eb4a9717f4c3722
Curtis Lassam <curtis@lassam.net>
2019-02-14 15:36:03 -0800

    fix invoke problem

--- tasks.py
+++ tasks.py
@@ -2,13 +2,13 @@ from invoke import run, task
 import pkg_resources
 
 @task
-def test():
+def test(ctx):
     # /c/Python27/Scripts/nosetests.exe --with-doctest
-    run("nosetests --with-doctest")
+    ctx.run("nosetests --with-doctest")
 
 @task
-def release():
+def release(ctx):
     version = pkg_resources.require("silly")[0].version
     print("Release: {release}".format(release=version))
-    run("python setup.py bdist_wheel --universal")
-    run("twine upload dist/silly-{release}-py2.py3-none-any.whl".format(release=version))
+    ctx.run("python setup.py bdist_wheel --universal")
+    ctx.run("twine upload dist/silly-{release}-py2.py3-none-any.whl".format(release=version))
